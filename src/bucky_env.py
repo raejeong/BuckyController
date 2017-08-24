956e4ee0b870e632595b7cfa5d47bb1e15d4ec4e
rae.jeong <rae.jeong@istuary.com>
2017-08-24 15:42:26 -0700

    pid with encoder

--- src/bucky_env.py
+++ src/bucky_env.py
@@ -1,6 +1,7 @@
 import serial
 import math
 import time
+import numpy as np
 
 bucky_serial = serial.Serial('/dev/ttyACM0',115200)
 time.sleep(2)
@@ -32,11 +33,13 @@ class BuckyEnv():
 	def __init__(self):
 		self.actions = {"jump":0.0, "motor1":0.0, "motor2":0.0}
 		self.action_dimensions = 3
-		self.action_bounds = {"jump": [0.0,1.0,0.0,255.0], "motor1":[-1.0,1.0,-30.0,30.0], "motor2":[-1.0,1.0,-30.0,30.0]}
+		self.action_bounds = {"jump": [0.0,1.0,0.0,250.0], "motor1":[-1.0,1.0,-500.0,500.0], "motor2":[-1.0,1.0,-500.0,500.0]}
 		self.state = [0.0]*8
 		self.state_dimensions = 8
 		self._data_flag = "999"
 		self._action_names = ["jump", "motor1", "motor2"]
+		self.x_ref = [0.0]*100
+		self.x_avg = [0.0]*10
 
 	def update_state(self):
 		state_string = bucky_serial.readline()
@@ -44,8 +47,16 @@ class BuckyEnv():
 		if state_string[0] == "1":
 			raw_state = list(map(float,state_string))[1:]
 			x_angle, y_angle, z_angle = Quaternion_toEulerianAngle(raw_state[3],raw_state[4],raw_state[5],raw_state[6])
+			self.x_avg.pop(0)
+			self.x_avg.append(x_angle)
+			self.x_ref.pop(0)
+			self.x_ref.append(x_angle)
 			self.state[:3] = raw_state[:3]
-			self.state[3] = x_angle
+			# self.state[3] = sum(self.x_avg)/len(self.x_avg)
+			self.state[3] = x_angle 
+			offset = sum(self.x_ref)/len(self.x_ref)
+			if np.abs(offset) > 0.05:
+				self.state[3] -= offset
 			self.state[4] = y_angle
 			self.state[5] = z_angle
 			self.state[6:] = raw_state[7:]
--- src/bucky_pid.py
+++ src/bucky_pid.py
@@ -1,49 +1,68 @@
 #!/usr/bin/env python
 import bucky_env
 import numpy as np
+import simple_plot
+import time
 
 action = {"jump":0.0, "motor1":0.0, "motor2":0.0}
 
 def compute_action(state):
-	x_ref = 0.12
-	y_ref = -0.10
-	x_KP = 5.0
-	y_KP = 5.0
-	x_angle = state[4]
-	y_angle = state[3]
-	x_err = x_ref - x_angle
-	y_err = y_ref - y_angle
-	# action["motor1"] = x_err
-	action["motor1"] = state[6]/30.0 + x_KP*x_err
-	# if abs(x_err) > 0.05:
-		# action["motor1"] = state[6]/30.0 + x_KP*x_err + 0.2
-		# action["motor1"] = x_KP*x_err
-	# action["motor2"] += y_KP*y_err
-	if action["motor1"] > 1.0:
-		action["motor1"] = 1.0
-	if action["motor1"] < -1.0:
-		action["motor1"] = -1.0
-	if action["motor2"] > 1.0:
-		action["motor2"] = 1.0
-	if action["motor2"] < -1.0:
-		action["motor2"] = -1.0
-	if (np.abs(x_err) > 0.26) or (np.abs(y_err) > 0.26):
-		action["motor1"] = 0.0
-		action["motor2"] = 0.0
+    x_ref = 0.0
+    y_ref = 0.0
+    x_angle = state[3]
+    y_angle = state[4]
+    x_err = x_ref - x_angle
+    y_err = y_ref - y_angle
+    with open("k_value.txt", "r") as txt:
+        x_KP = float(txt.readline())
+    # action["motor1"] = x_err
+    action["motor1"] = -1.0*state[6]/500.0 + x_KP*x_err
+    # print x_KP
+    # if np.abs(action["motor1"]) <  0.001:
+    #   action["motor1"] = 0.0
+    # action["motor2"] = y_KP*y_err
 
-	return action, x_err, y_err
+    # action["motor2"] = state[7]/30.0 + y_KP*y_err
+
+    # if abs(x_err) > 0.05:
+        # action["motor1"] = state[6]/30.0 + x_KP*x_err + 0.2
+        # action["motor1"] = x_KP*x_err
+    # action["motor2"] += y_KP*y_err
+    if action["motor1"] > 1.0:
+        action["motor1"] = 1.0
+    if action["motor1"] < -1.0:
+        action["motor1"] = -1.0
+    if action["motor2"] > 1.0:
+        action["motor2"] = 1.0
+    if action["motor2"] < -1.0:
+        action["motor2"] = -1.0
+    if (np.abs(x_err) > 0.2) or (np.abs(y_err) > 0.2):
+        action["motor1"] = 0.0
+        action["motor2"] = 0.0
+
+    return action, x_err, y_err
 
 def main():
-   env = bucky_env.BuckyEnv()
-   while(True):
-   	success = env.update_state()
-   	if success:
-   		state = env.state
-   		action, x_err, y_err = compute_action(state)
-   		env.take_action(action)
-   		formatted_state = [ '%.2f' % elem for elem in state ]
-   		print "state, action: ", formatted_state, action, [x_err, y_err]
-   		# print "action: ", action
+    env = bucky_env.BuckyEnv()
+    simple_plotter = simple_plot.SimplePlot(3)
+    last_time = 0.0
+    while(True):
+        # print "delta T: ", (time.time()-last_time)
+        last_time = time.time()
+        success = env.update_state()
+        if success:
+            state = env.state
+            action, x_err, y_err = compute_action(state)
+            env.take_action(action)
+            formatted_state = [ '%.2f' % elem for elem in state ]
+
+            print "state, action, ref, k: ", formatted_state, action, [x_err, y_err]
+            data_to_plot = [x_err, action['motor1'], state[6]/200.0]
+            # print "action: ", action
+        else:
+            data_to_plot = [0.0, 0.0, 0.0]
+        simple_plotter.update(data_to_plot)
+
 
 if __name__ == '__main__':
     main()
